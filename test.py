37c3f37a679a06f79ffdcc0e94b9f094ad45704b
GiftMood <1449453442@qq.com>
2020-05-24 11:04:35 +0800

    SECORD

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ conn=sqlite3.connect("Bookstore.db")
 cur=conn.cursor()
 
 
-cur.execute('select * from book ')
+cur.execute("select * from users where username ='5DFB8AAM'")
 rs=cur.fetchall()
+print(rs)
 all_time = 0
 for line in rs:
     all_time += 1
+    print(line)
 print("总共的条目数量:%d"%(all_time))
 conn.commit()
 conn.close()
---
+++ usersystem.py
@@ -0,0 +1,79 @@
+import string
+import sqlite3
+
+def registered(username,password):
+    conn = sqlite3.connect("Bookstore.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users where username = '{}'".format(username))
+    rs = cur.fetchall()
+    if(rs == []):
+        row = [username,password,"0","0.0"]
+        sql = "insert into users values('{0[0]}','{0[1]}','{0[2]}'" \
+              ",'{0[3]}')".format(row)
+        print(sql)
+        try:
+            cur.execute(sql)
+        except:
+            conn.commit()
+            conn.close()
+            return False
+        '''
+        cur.execute("select * from users where username ='5DFB8AAM'")
+        rs = cur.fetchall()
+        print(rs)
+        '''
+        conn.commit()
+        conn.close()
+        return True
+
+    else:
+        conn.commit()
+        conn.close()
+        return False
+
+def checkpassword(username,password):
+    conn = sqlite3.connect("Bookstore.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users where username = '{}' and password = '{}'".format(username,password))
+    rs = cur.fetchall()
+    if(rs ==[]):
+        conn.commit()
+        conn.close()
+        return False
+    else:
+        conn.commit()
+        conn.close()
+        return True
+#print(checkpassword('5DFB8AAH','80764540'))
+def join_membership(username):
+    conn = sqlite3.connect("Bookstore.db")
+    cur = conn.cursor()
+    cur.execute("update users set level ='1' where username = '{}'".format(username))
+    cur.execute("select * from users where username ='{}' and level = '{}'".format(username,"1"))
+    rs = cur.fetchall()
+    if(rs ==[]):
+        conn.commit()
+        conn.close()
+        return False
+    else:
+        #print(rs)
+        conn.commit()
+        conn.close()
+        return True
+
+#print(join_membership('5DFB8AAH'))
+
+def checkvip(username):
+    conn = sqlite3.connect("Bookstore.db")
+    cur = conn.cursor()
+    cur.execute("select * from users where username ='{}' and level = '{}'".format(username, "1"))
+    rs = cur.fetchall()
+    if (rs == []):
+        conn.commit()
+        conn.close()
+        return False
+    else:
+        # print(rs)
+        conn.commit()
+        conn.close()
+        return True
